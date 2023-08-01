40a607600af28c2a78af581f50d171896d28fd80
Dmitry552 <focker552@gmail.com>
2023-08-01 22:55:03 +0300

    added color theme change

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
